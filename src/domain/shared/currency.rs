e714f4224e60c7edad30102a0e25d1ffb394fb99
AraMinjibir <abubakar.aramjb@gmail.com>
2026-07-02 22:29:15 +0100

    feat(payment): add money value object

--- src/domain/shared/currency.rs
+++ src/domain/shared/currency.rs
@@ -1,9 +1,11 @@
-#[derive(Debug, Clone)]
+use strum_macros::{Display, EnumString};
+
+#[derive(Debug, Clone, Display, EnumString)]
 pub enum Currency {
     NGN,
     USD,
     EUR,
     GBP,
     AED,
-    SAR
+    SAR,
 }
