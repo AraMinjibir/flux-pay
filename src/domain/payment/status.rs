0bfd2c6e2e3add233ba90f9c416d10ef9b9ae113
AraMinjibir <abubakar.aramjb@gmail.com>
2026-07-08 20:09:58 +0100

    chore(status): add PartialEq trait

--- src/domain/payment/status.rs
+++ src/domain/payment/status.rs
@@ -1,7 +1,7 @@
 use serde::{Deserialize, Serialize};
 use strum_macros::{Display, EnumString};
 
-#[derive(Debug, Clone, Serialize, Deserialize, Display, EnumString)]
+#[derive(Debug, Clone, Serialize, Deserialize, Display, EnumString, PartialEq)]
 #[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
 pub enum PaymentStatus {
     Created,
