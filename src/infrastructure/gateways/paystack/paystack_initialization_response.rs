77464a96d5e6a6a995e612a0ef0fc5afec370232
AraMinjibir <abubakar.aramjb@gmail.com>
2026-07-10 22:42:22 +0100

    feat(paystack): add  paystack  provider intialization response

---
+++ src/infrastructure/gateways/paystack/paystack_initialization_response.rs
@@ -0,0 +1,28 @@
+use serde::Deserialize;
+
+use crate::domain::payment::{payment::PaymentInitializationResult, status::PaymentStatus};
+
+#[derive(Deserialize)]
+pub struct PaystackInitializeResponse {
+    status: PaymentStatus,
+    message: String,
+    data: PaystackData,
+}
+
+#[derive(Deserialize)]
+pub struct PaystackData {
+    authorization_url: String,
+    access_code: String,
+    reference: String,
+}
+
+impl From<PaystackInitializeResponse> for PaymentInitializationResult {
+    fn from(response: PaystackInitializeResponse) -> Self {
+        Self {
+            provider_reference: response.data.reference,
+            authorization_url: Some(response.data.authorization_url),
+            status: response.status,
+            client_secret: None,
+        }
+    }
+}
