f4c390b91af175945e067cc21beb7be5099b16b8
AraMinjibir <abubakar.aramjb@gmail.com>
2026-07-21 12:19:20 +0100

    error(repo): introduce a mapper for repository errors and orchestration metadata

--- src/domain/errors/domain_error.rs
+++ src/domain/errors/domain_error.rs
@@ -1,6 +1,6 @@
 use std::fmt;
 
-use crate::domain::payment::{provider::PaymentProvider, status::PaymentStatus};
+use crate::{application::payment_orchestrator::{OrchestrationFailureMetadata}, domain::{errors::repository_error::RepositoryError, payment::{provider::PaymentProvider, status::PaymentStatus}}};
 
 #[derive(Debug)]
 pub enum DomainError {
@@ -13,8 +13,14 @@ pub enum DomainError {
     ProviderUnavailable,
     ProviderNotFound(PaymentProvider),
     NoProviderAvailable,
-
+    PaymentProviderFailed {
+        error: Box<DomainError>,
+        metadata: OrchestrationFailureMetadata,
+    },
+    
     UnsupportedCurrency,
+    RequestInProgress,
+    InvalidStoredResponse,
 
     InvalidPaymentStatusTransition {
         from: PaymentStatus,
@@ -22,6 +28,20 @@ pub enum DomainError {
     },
 
     DatabaseError(String),
+    DuplicateEntity,
+    ForeignKeyViolation,
+    EntityNotFound,
+    ConnectionError,
+    PoolTimeout,
+    NullConstraintViolation,
+    CheckConstraintViolation,
+    DataTooLong,
+    InvalidDataFormat,
+    NumericOverflow,
+    DeadlockDetected,
+    TransactionTimeout,
+    SerializationFailure,
+    Internal(String)
 }
 
 impl DomainError {
@@ -66,9 +86,72 @@ impl fmt::Display for DomainError {
                     from, to
                 )
             }
+            DomainError::RequestInProgress => {
+                write!(f, "This request is already received and currently Processing")
+            }
+            DomainError::InvalidStoredResponse => {
+                write!(f, "Invalid stored response")
+            }
             DomainError::DatabaseError(msg) => {
                 write!(f, "Database error: {}", msg)
             }
+
+            DomainError::ForeignKeyViolation => {
+                write!(f, "Foreign key violation")
+            }
+
+            DomainError::NullConstraintViolation => {
+                write!(f, "Null constraint violation")
+            }
+
+            DomainError::CheckConstraintViolation => {
+                write!(f, "Check constraint violation")
+            }
+
+            DomainError::DataTooLong => {
+                write!(f, "Data too long")
+            }
+
+            DomainError::InvalidDataFormat => {
+                write!(f, "Invalid data format")
+            }
+
+            DomainError::NumericOverflow => {
+                write!(f, "Numeric overflow")
+            }
+
+            DomainError::DeadlockDetected => {
+                write!(f, "Deadlock detected")
+            }
+
+            DomainError::TransactionTimeout => {
+                write!(f, "Transaction timeout")
+            }
+
+            DomainError::SerializationFailure => {
+                write!(f, "Serialization failure")
+            }
+
+            DomainError::DuplicateEntity => {
+                write!(f, "Duplicate Entity")
+            }
+            DomainError::EntityNotFound => {
+                write!(f, "Entity Not Found")
+            }
+            DomainError::ConnectionError => {
+                write!(f, "Connection Error")
+            }
+            DomainError::PoolTimeout => {
+                write!(f, "Pool Timeout")
+            }
+            DomainError::Internal(msg) => {
+                write!(f, "Internal system error: {}", msg)
+            }
+
+            DomainError::PaymentProviderFailed { error, .. } => {
+                write!(f, "Payment provider failed: {error}")
+            }
+
         }
     }
 }
@@ -86,5 +169,25 @@ impl From<redis::RedisError> for DomainError {
 impl From<serde_json::Error> for DomainError {
     fn from(err: serde_json::Error) -> Self {
         DomainError::DatabaseError(err.to_string())
+    }
+}
+impl From<RepositoryError> for DomainError {
+    fn from(err: RepositoryError) -> Self {
+        match err {
+            RepositoryError::DuplicateEntity => DomainError::DuplicateEntity,
+            RepositoryError::ForeignKeyViolation => DomainError::ForeignKeyViolation,
+            RepositoryError::NullConstraintViolation => DomainError::NullConstraintViolation,
+            RepositoryError::CheckConstraintViolation => DomainError::CheckConstraintViolation,
+            RepositoryError::DataTooLong => DomainError::DataTooLong,
+            RepositoryError::EntityNotFound => DomainError::EntityNotFound,
+            RepositoryError::PoolTimeout => DomainError::PoolTimeout,
+            RepositoryError::InvalidDataFormat => DomainError::InvalidDataFormat,
+            RepositoryError::ConnectionError => DomainError::ConnectionError,
+            RepositoryError::NumericOverflow => DomainError::NumericOverflow,
+            RepositoryError::DeadlockDetected => DomainError::DeadlockDetected,
+            RepositoryError::TransactionTimeout => DomainError::TransactionTimeout,
+            RepositoryError::SerializationFailure => DomainError::SerializationFailure,
+            RepositoryError::DatabaseError(msg) => DomainError::DatabaseError(msg),
+        }
     }
 }
