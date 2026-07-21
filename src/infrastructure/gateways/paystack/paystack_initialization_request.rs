66cc25e9337996adbe7ea24ac246e05d9e4b1caf
AraMinjibir <abubakar.aramjb@gmail.com>
2026-07-21 12:32:42 +0100

    style: apply Rust style

--- src/infrastructure/gateways/paystack/paystack_initialization_request.rs
+++ src/infrastructure/gateways/paystack/paystack_initialization_request.rs
@@ -4,21 +4,21 @@ use crate::domain::payment::payment::PaymentInitializationRequest;
 
 #[derive(Serialize)]
 pub struct PaystackInitializeRequest {
-    pub email: String,
+    pub email: Option<String>,
     pub amount: i64,
     pub currency: String,
     pub reference: String,
-    pub callback_url: String,
+    pub callback_url: Option<String>,
 }
 
 impl From<&PaymentInitializationRequest> for PaystackInitializeRequest {
     fn from(payment: &PaymentInitializationRequest) -> Self {
         Self {
-            email: payment.email().to_string(),
-            amount: payment.amount(),
-            currency: payment.currency().to_string(),
-            reference: payment.reference().to_string(),
-            callback_url: payment.callback_url().to_string(),
+            email: payment.email.clone(),
+            amount: payment.amount,
+            currency: payment.currency.to_string(),
+            reference: payment.reference.to_string(),
+            callback_url: payment.callback_url.clone(),
         }
     }
 }
