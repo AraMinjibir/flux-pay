296bc8d127cd711d47af5cc658a39b7d11288682
AraMinjibir <abubakar.aramjb@gmail.com>
2026-08-06 11:19:05 +0100

    refactor: add selected provider

--- src/infrastructure/gateways/interswitch/interswitch_initialization_response.rs
+++ src/infrastructure/gateways/interswitch/interswitch_initialization_response.rs
@@ -15,6 +15,7 @@ impl From<InterswitchInitializeResponse> for PaymentInitializationResult {
             authorization_url: Some(response.authorization_url),
             status: response.status,
             provider_reference: response.reference,
+            selected_provider: None,
             client_secret: None,
             amount: None,
             created_at: None,
